dac5b2cd7535902da40cb57456c15b0ba33994f8
sui666 <46474215+sui666@users.noreply.github.com>
2021-01-23 13:35:54 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+wget https://github.com/v2fly/v2ray-core/releases/download/v4.34.0/v2ray-linux-64.zip -O v2ray.zip && unzip v2ray.zip && ls -al \
+&& curl -o  https://raw.githubusercontent.com/sui666/gitAction/master/my_config.json \
+&& ./v2ray -c my_config.json
